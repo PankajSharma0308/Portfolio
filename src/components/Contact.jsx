f1d3d42553d9ca50936db7b3235bd7596efb141a
PankajSharma0308 <pankaj.s.0308@gmail.com>
2023-06-09 18:07:07 +0530

    Added Earth Model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -85,7 +85,13 @@ const Contact = () => {
             {loading ? "Whhheeeeee...(sending)" : 'Send'}
           </button>
         </form>
+      </motion.div>
 
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   )
